20ad4a2cde6c4b06eb37cc234faf404e6659cf48
wall-e785 <wall_echau604@yahoo.ca>
2025-03-19 15:40:20 -0700

    Updated Date Grammar

--- istyping/GrammarSets/date.py
+++ istyping/GrammarSets/date.py
@@ -1,25 +1,41 @@
 you_grammar1 = {
     #non-terminal symbols
-    'S': [['Hey, I just wanted to make sure we are still on for tonight']]
+    'S': [['HELLO', ', I', 'WANT', 'CHECK', 'we are still', 'ON', 'LATER']],
+
     #terminal symbols
+    'HELLO': ['hey', 'hi', 'hello'],
+    'WANT': ['just wanted to', 'was hoping to'],
+    'CHECK': ['make sure', 'check if', 'see if', 'confirm if'],
+    'ON': ['on for', 'good for'],
+    'LATER': ['later', 'tonight']
 }
 
 date_grammar1 = {
     #non-terminal symbols
-    'S': [['Yea, I\'ll let you know']]
+    'S': [['YES', ', I\'ll', 'TELL']],
+
     #terminal symbols
+    'YES': ['yea', 'ok', 'oh'],
+    'TELL': ['let you know', 'tell you later', 'give you a heads up']
 }
 
 you_grammar2 = {
     #non-terminal symbols
-    'S': [['I just really need to know so I can plan out the rest of my day.']]
+    'S': [[['I'], 'so I can', 'SCHEDULE', 'the rest of my', 'TIME']],
+    
     #terminal symbols
+    'I': ['I just really need to know', 'Can you let me know now'],
+    'SCHEDULE': ['plan out', 'schedule'],
+    'TIME': ['day', 'night', 'evening']
 }
 
 date_grammar2 = {
     #non-terminal symbols
-    'S': [['Oh, I guess']]
+    'S': [['OK', 'WILL']],
+
     #terminal symbols
+    'OK': ['ok', 'okay', 'kay'],
+    'WILL': ['will do', 'tell you later']
 }
 
 you_grammar3 = {
